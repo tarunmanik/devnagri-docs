57df9c9f0fa49f636d4249844425aa8c3492fde0
kumarravisingh <kumarravi@fourtek.com>
2018-05-01 19:18:01 +0530

    added sh file

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
- echo 'support.devnagri.com' > CNAME
+ #echo 'support.devnagri.com' > CNAME
 
 git init
 git add -A
