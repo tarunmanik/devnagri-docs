b0557f1054977e7a55efe0909c60766666523adf
kumarravisingh <kumarravi@fourtek.com>
2018-05-02 18:46:49 +0530

    vuepress deploy commit

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd .vuepress/dist
+cd .vuepress
 
 # if you are deploying to a custom domain
  #echo 'support.devnagri.com' > CNAME
